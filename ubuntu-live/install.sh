1951aa85785645d2223557aa204591c355b97311
antnks <antnks@users.noreply.github.com>
2020-01-13 09:37:26 +0200

    Added ubuntu multiverse and universe repos

--- ubuntu-live/install.sh
+++ ubuntu-live/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo add-apt-repository universe
+sudo add-apt-repository multiverse
 sudo apt-get update
 sudo apt-get install git make bridge-utils net-tools gddrescue hostapd openvpn
 
