8891990d1682fa8d5fa9e0fbed5969c6a491c1d6
antnks <antnks@users.noreply.github.com>
2020-01-13 10:17:57 +0200

    Typo in apt-get

--- ubuntu-live/install.sh
+++ ubuntu-live/install.sh
@@ -5,7 +5,7 @@ sudo add-apt-repository multiverse
 sudo apt-get update
 
 sudo apt-get install git make vim gddrescue
-sudo apt0get install bridge-utils net-tools hostapd openvpn wireshark
+sudo apt-get install bridge-utils net-tools hostapd openvpn wireshark
 
 sudo apt-get install default-jre
 wget "https://portswigger.net/burp/releases/download?product=community&version=1.7.36&type=jar&componentid=100" -O burp.jar
