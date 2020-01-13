e4cb7db869933db0557b23b87289a7cc85cc5488
antnks <antnks@users.noreply.github.com>
2020-01-13 09:40:19 +0200

    Added burp download

--- ubuntu-live/install.sh
+++ ubuntu-live/install.sh
@@ -5,3 +5,6 @@ sudo add-apt-repository multiverse
 sudo apt-get update
 sudo apt-get install git make bridge-utils net-tools gddrescue hostapd openvpn
 
+sudo apt-get install default-jre
+wget "https://portswigger.net/burp/releases/download?product=community&version=1.7.36&type=jar&componentid=100" -O burp.jar
+
